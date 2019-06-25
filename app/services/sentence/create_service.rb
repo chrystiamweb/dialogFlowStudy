416fb1475d1f82fa571cc6503b1c644a2d384f16
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-06-25 17:05:13 +0000

    changed translate service to process pt-en and en-pt

--- app/services/sentence/create_service.rb
+++ app/services/sentence/create_service.rb
@@ -2,15 +2,27 @@ module SentenceModule
   class TranslateService
     include HTTParty
     def initialize(params)
-      @sentence = params["sentence"]
+      puts params
+      @sentence = ''
+      @lang = ''
+      set_lang(params)
       @apikey = 'trnsl.1.1.20190618T140151Z.86043cc92b92b6be.e922fe465d1f644377f6f45d900cf5f1fd3b0cf0'
-    end
- 
+    end 
     
     def call
       base_uri = "https://translate.yandex.net/api/v1.5/tr.json/translate?"
-      result = self.class.get("#{base_uri}key=#{@apikey}&text='#{@sentence}'&lang=pt-en")
-      result['text'][0]
+      result = self.class.get("#{base_uri}key=#{@apikey}&text='#{@sentence}'&lang=#{@lang}")
+      result['text'][0]    
+    end
+
+    def set_lang(request)
+      if request['sentence'].empty?     
+        @sentence = request['language']
+        @lang = 'pt-en' 
+      else
+        @sentence = request['sentence']
+        @lang = 'en-pt'
+      end
     end
   end
 end
